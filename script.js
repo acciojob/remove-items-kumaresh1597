5b5054b2943aa3673f346cbb635a30d4f730623c
Kumaresh <kumaresh.1597@gmail.com>
2023-08-18 17:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeSelected() {
+    let selectedElements = document.querySelectorAll("#colorSelect");
+    let value = selectedElements[0].value;
+
+	let options = document.getElementsByTagName("option");
+    for(let i=0;i<options.length;i++){
+        if(options[i].innerText == value){
+            options[i].remove();
+        }
+    }
+}
